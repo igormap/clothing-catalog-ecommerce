49a87e026631408b6e16d6a5e6f7735bfe89b49c
Igor Martins <igormartins16@gmail.com>
2025-01-13 21:30:53 -0400

    feat: item card component

---
+++ src/app/components/ItemCard.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+
+export interface Product {
+  id: string;
+  name: string;
+  category: string;
+  discount_percentage?: number;
+  promotion_price?: number;
+  price: number;
+  image: string;
+  description: string;
+}
+
+interface Props {
+  product: Product;
+  onAddToCart: (product: Product) => void;
+}
+
+export function ItemCard({ product, onAddToCart }: Props) {
+  // const { promotion_price, discount_percentage } = product;
+
+  return (
+    <div className="w-72 h-[440px] flex flex-col bg-slate-200 relative group">
+      <div className="w-full h-full flex items-center justify-center absolute z-50 bg-gray-700 bg-opacity-75 invisible group-hover:visible">
+        <button
+          className="w-52 h-12 opacity-100 bg-opacity-100 bg-white text-amber-800"
+          onClick={onAddToCart}
+        >
+          Adicionar ao carrinho
+        </button>
+      </div>
+      <div className="flex justify-center items-center absolute rounded-full bg-red-400 w-12 h-12 text-white top-4 right-4 z-40">
+        <span>-30%</span>
+      </div>
+      <div className="h-2/3 w-full relative">
+        <Image
+          alt="Item"
+          src="https://s3-sa-east-1.amazonaws.com/prod-superon-public-media/prova-frontend/tshirts/tshirt-1.jpg"
+          fill
+          objectFit="cover"
+        />
+      </div>
+      <div className="flex flex-col gap-2 px-4 py-4">
+        <h3 className="h-1/3 text-2xl font-semibold text-gray-800">Camisa</h3>
+        <span className="text-base text-gray-500">Camisa verde bonita</span>
+        <div className="flex gap-4 items-center">
+          <span className="text-xl font-semibold text-gray-800">R$ 200</span>
+          <span className="text-gray-400 line-through">R$ 250</span>
+        </div>
+      </div>
+    </div>
+  );
+}
