8163e8cf7ecfcb514c3945c822a2384d83578d40
Igor Martins <igormartins16@gmail.com>
2025-01-17 19:24:37 -0400

    feat: add cart product items count in header

--- src/app/components/Herosection.tsx
+++ src/app/components/Herosection.tsx
@@ -1,31 +1,24 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export function HeroSection() {
   return (
-    <div className="relative w-full h-screen overflow-hidden">
+    <div className="relative w-full h-96 overflow-hidden">
       <div className="absolute top-0 left-0 w-full h-full">
         <Image
-          src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          src="https://images.unsplash.com/photo-1490481651871-ab68de25d43d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="hero"
           layout="fill"
           objectFit="cover"
         />
       </div>
-      <div className="absolute top-0 left-0 w-full h-full bg-black/50" />
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+      <div className="absolute top-0 left-0 w-full h-full bg-black/30" />
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center flex items-center flex-col">
         <h1 className="text-4xl font-bold text-white mb-4">
           Bem vindo a nossa loja
         </h1>
-        <p className="text-lg text-white mb-8">
+        <p className="text-lg text-white">
           Compre produtos de qualidade com confiança
         </p>
-        <Link
-          href="#"
-          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
-        >
-          Shop Now
-        </Link>
       </div>
     </div>
   );
